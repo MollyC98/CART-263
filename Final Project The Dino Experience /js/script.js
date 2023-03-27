9f6e3b03c609221cfb1437166105bb5403bb19a4
MollyC98 <chakrabortymollika@gmail.com>
2023-03-26 21:15:36 -0400

    Final project
    The Dino Experience

---
+++ Final Project The Dino Experience /js/script.js
@@ -0,0 +1,24 @@
+/**
+Final Project, The Dino Experience 
+Negar and Mollika 
+
+This is a template. You must fill in the title,
+author, and this description to match your project!
+*/
+
+"use strict";
+
+
+
+function preload() {
+ 
+}
+function setup() {
+ 
+ }
+
+
+
+function draw() {
+
+}
